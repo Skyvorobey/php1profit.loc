9b4d404a832ab3d97b0922a1a819cac6d1f6d9c5
Sky <skyvorobey@icloud.com>
2020-12-19 16:48:01 +0200

    минимизировал и исправил код

--- dz4/scripts/form_upload/form_upload.php
+++ dz4/scripts/form_upload/form_upload.php
@@ -1,25 +1,14 @@
 <?php
-if (isset($_FILES['file_name'])) {
-    $file_weight = $_FILES['file_name']['size'];
-    $file_original_name = $_FILES['file_name']['name'];
-    $file_tmp_name = $_FILES['file_name']['tmp_name'];
-    $file_type = $_FILES['file_name']['type'];
-    $file_error = $_FILES['file_name']['error'];
+if ((isset($_FILES['file_name'])) && ($_FILES['file_name']['error'] === 0) && (('image/png' === $_FILES['file_name']['type']) || (('image/jpeg' === $_FILES['file_name']['type'])))) {
 
     $images_url = __DIR__ . '/../../images/';
 
-    if ((0 === $file_error) && (('image/png' === $file_type) || (('image/jpeg' === $file_type)))) {
-       move_uploaded_file($file_tmp_name, $images_url . $file_original_name);
-       echo 'Файл успешно загружен!';
+    move_uploaded_file($_FILES['file_name']['tmp_name'], $images_url . $_FILES['file_name']['name']);
+    echo 'Файл успешно загружен!';
 
-        $host  = $_SERVER['HTTP_HOST'];
-        $uri   = rtrim(dirname($_SERVER['PHP_SELF']), '/\\');
-        $extra = 'index.php';
-        header("Location: http://$host$uri/$extra");
-
-    } else {
-        echo 'Формат загружаемых файлов должен быть jpeg или png!';
-    }
+    header("Location: /dz4/index.php");
+}   else {
+    echo 'Формат загружаемых файлов должен быть jpeg или png.';
 }
 ?>
 
