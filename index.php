a4021479648589cf15a7eeed1352935b32db1163
Sky <skyvorobey@icloud.com>
2020-12-19 13:20:38 +0200

    Добавил навигацию

---
+++ index.php
@@ -0,0 +1,8 @@
+<a href="/dz1/index.php">№1</a><br>
+<a href="/dz2/index.php">№2</a><br>
+<a href="/dz3/index.php">№3</a><br>
+<a href="/dz4/index.php">№4</a><br>
+<a href="/dz5/index.php">№5</a><br>
+<a href="/dz6/index.php">№6</a><br>
+<a href="/dz7/index.php">№7</a><br>
+
