dc91ef139d529093c36eaa2b7dc93db917edd2a5
Sky <skyvorobey@icloud.com>
2020-12-19 15:51:28 +0200

    поправил скрипт обработчик

--- dz4/scripts/guest_book/script_book.php
+++ dz4/scripts/guest_book/script_book.php
@@ -1,20 +1,14 @@
 <?php
-if (isset ($_POST['submit'])) {
-    if (($_POST['new_rec']) !== '') {
-        $file = fopen(__DIR__ . '/../../data/guest_book.txt', 'a');
-        $new_r = ($_POST['new_rec']);
-        fwrite($file, date('d.m.Y H:i:s', time())) .
-        fwrite($file, ', ') .
-        fwrite($file, $new_r) .
-        fwrite($file, '; ') .
-        fclose($file);
+if (isset ($_POST['submit']) && (($_POST['new_rec']) !== '') && (($_POST['new_rec']) !== null)) {
+    $file = fopen(__DIR__ . '/../../data/guest_book.txt', 'a');
+    $new_r = ($_POST['new_rec']);
+    fwrite($file, date('d.m.Y H:i:s', time())) .
+    fwrite($file, ', ') .
+    fwrite($file, $new_r) .
+    fwrite($file, '; ') .
+    fwrite($file, "\n") .
+    fclose($file);
 
-        $host  = $_SERVER['HTTP_HOST'];
-        $uri   = rtrim(dirname($_SERVER['PHP_SELF']), '/\\');
-        $extra = 'guest_book.php';
-        header("Location: http://$host$uri/$extra");
-    } else {
-        echo 'Необходимо заполните поле!';
-    }
+    header("Location: /dz4/guest_book.php");
 }
 ?>
